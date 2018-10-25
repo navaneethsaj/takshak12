8e04926f4255f390a43f3c342ed71ba5bcf505d9
Navaneeth <navaneethsaj@gmail.com>
2018-10-25 22:14:36 +0530

    final touches

---
+++ app/src/main/java/takshak/mace/takshak2k18/devs.java
@@ -0,0 +1,13 @@
+package takshak.mace.takshak2k18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class devs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devs);
+    }
+}
